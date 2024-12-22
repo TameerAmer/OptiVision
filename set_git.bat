29a64bbdbb703dbc55f2b1f5c3c159bb88464e75
TameerAmer <tameergti@gmail.com>
2024-12-22 20:08:48 +0200

    first commit

--- set_git.bat
+++ set_git.bat
@@ -9,4 +9,4 @@ git add .
 git commit -m "first commit"
 git branch -M main
 ::git remote add origin https://github.com/TameerAmer/FinalProject.git
-git push -u origin main
+git push -u origin main --force
